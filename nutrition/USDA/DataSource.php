3f15f48edde30ca1f5f17e335cb9edcda1d414d6
daniel.oconnor <daniel.oconnor@e8bfc7de-1b4f-0410-ac97-210d4ce62743>
2009-01-12 22:04:46 +0000

    Added the ability to resolve IDs

--- nutrition/USDA/DataSource.php
+++ nutrition/USDA/DataSource.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'Base.php';
+require_once 'Net/URL2.php';
 
 class USDA_DataSource extends USDA_Base {
     public $datasrc_id;
@@ -47,9 +48,55 @@ class USDA_DataSource extends USDA_Base {
             issue_state VARCHAR(5),
             start_page INT(5),
             end_page INT(5),
+            openurl TEXT
 
             KEY `datasrc_id` (`datasrc_id`)
         )';
     }
 
+    public function getBioGUIDLink() {
+        if (empty($this->volume_city) || empty($this->start_page) || empty($this->journal)) {
+            return false;
+        }
+
+        $url = new Net_URL2('http://bioguid.info/openurl');
+
+        $url->setQueryVariable('genre', 'article');
+
+        $url->setQueryVariable('title', $this->journal);
+        $url->setQueryVariable('volume', $this->volume_city);
+        $url->setQueryVariable('spage', $this->start_page);
+    
+        $url->setQueryVariable('display', 'rdf');
+
+        return $url->getURL();
+    }
+
+    public function resolve() {
+        $link = $this->getBioGUIDLink();
+
+        if (empty($link)) {
+            return false;
+        }
+
+        $document = simplexml_load_file($link);
+        $namespaces = $document->getNamespaces(true);
+
+        //Register them with their prefixes
+        foreach ($namespaces as $prefix => $ns) {
+            $document->registerXPathNamespace($prefix, $ns);
+        }
+
+        list($id) = $document->xpath('//dc:identifier');
+
+        if (!empty($id)) {
+            $link = 'http://bioguid.info/' . $id;
+            $sql = "UPDATE data_sources SET openurl = " . $this->db->quote($link) . " WHERE datasrc_id = " . $this->db->quote($this->getID());
+            $this->db->query($sql);
+
+            return $link;
+        }
+
+        return false;
+    }
 }
